ae450fb240b06404ad8b8cea7bf926df84888203
fischuu <github@danielfischer.name>
2016-06-14 18:16:47 +0300

    Improved summary.fa

--- R/summary.fa.R
+++ R/summary.fa.R
@@ -4,8 +4,11 @@ summary.fa <- function(object, ...){
    cat("Summary of fa object\n")
    cat("---------------\n")
    cat("Sequences      :",length(object),"\n")
-   cat("Average length :",mean(nCharObj),"\n")
    cat("Minimum length :",min(nCharObj),"\n")
+   cat("1st quartile   :",quantile(nCharObj, 0.25),"\n")
+   cat("Median length  :",median(nCharObj),"\n")
+   cat("Average length :",mean(nCharObj),"\n")
+   cat("3rd quartile   :",quantile(nCharObj, 0.75),"\n")
    cat("Maximum length :",max(nCharObj),"\n")
    invisible(object)
 
