83d58be9a5a065fc403064398eac70d9d5d7138c
fischuu <github@danielfischer.name>
2016-06-21 18:11:08 +0300

    annotationLabels adjusted

--- R/getAnnotation.R
+++ R/getAnnotation.R
@@ -2,19 +2,20 @@ cap <- function(x){
   paste(toupper(substring(x, 1, 1)), substring(x, 2), sep = "", collapse = " ")
 }
 
-getAnnotation <- function(species=NULL, release=84, version=NULL, tmpFolder){
+getAnnotation <- function(species=NULL, release=84, version=NULL, annotationFolder){
   if(is.null(version)){
     temp <- hoardeR::species
     version <- temp$Ensembl.Assembly[temp$Scientific.name==species]
   }
-  dir.create(tmpFolder)
+  
+  dir.create(annotationFolder, showWarnings=FALSE)
   species.int <- gsub(" ", "_",tolower(species))
   ensemblURL <- paste("ftp://ftp.ensembl.org/pub/release-",release,"/gtf/",species.int,"/",sep="")
   fileName <-  paste(cap(species.int),".",version,".",release,".chr.gtf.gz",sep="")
-  .file = file.path(tmpFolder, fileName)
+  .file = file.path(annotationFolder, fileName)
   
   # download file
-    download.file(paste(ensemblURL,fileName,sep=""), .file)
+  if(!file.exists(.file)) download.file(paste(ensemblURL,fileName,sep=""), .file)
   
   temp <- fread(input = paste('zcat',.file), skip=5, colClasses = c("character",
                                                                     "character",
--- R/tableSpecies.R
+++ R/tableSpecies.R
@@ -1,4 +1,4 @@
-tableSpecies <- function(xml, species=NULL, type="chr", minOutput=TRUE, exclude=""){
+tableSpecies <- function(xml, species=NULL, type="chr", minOutput=TRUE, exclude="", locations=FALSE){
   species.int <- species
   if(is.null(species.int)){
     species.int <- hoardeR::species$Scientific.name
@@ -8,17 +8,22 @@ tableSpecies <- function(xml, species=NULL, type="chr", minOutput=TRUE, exclude=
     xmlOne <- rbind(xmlOne,xml[[i]])
   }
   if(type=="chr") xmlOne <- xmlOne[!is.na(xmlOne$hitChr),]
-  xmlOne <- xmlOne[,1]
   
-  res <- c()
-  for(i in 1:length(species.int)){
-    res[i] <- sum(grepl(species.int[i],xmlOne))
-  }
-  names(res) <- species.int
-  if(minOutput)res <- res[res>0]
-  if(!is.null(exclude)){
-    remThese <- which(names(res)==exclude)
-    if(length(remThese)>0) res <- res[-remThese]
+  if(locations){
+    res <- xmlOne[grepl(species.int,xmlOne[,1]),]
+  } else {
+    xmlOne <- xmlOne[,1]
+    
+    res <- c()
+    for(i in 1:length(species.int)){
+      res[i] <- sum(grepl(species.int[i],xmlOne))
+    }
+    names(res) <- species.int
+    if(minOutput)res <- res[res>0]
+    if(!is.null(exclude)){
+      remThese <- which(names(res)==exclude)
+      if(length(remThese)>0) res <- res[-remThese]
+    }
   }
   res
 }
