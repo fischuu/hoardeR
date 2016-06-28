262d787fd6630507e15d017f58461d3f7f08b0be
fischuu <github@danielfischer.name>
2016-06-28 15:50:24 +0300

    Bug fixing for negative strand results

--- R/plotHit.R
+++ R/plotHit.R
@@ -5,14 +5,27 @@ plotHit <- function(hits, flanking=1, window=NULL, annot=TRUE, coverage=FALSE, l
                     origAnnot=NULL, hitAnnot=NULL, nTick=5, which=NULL, figureFolder=NULL,
                     figurePrefix=NULL){
   
+# Store the original values
+  windowOrig <- window
   hitsOrig <- hits
+  origAnnotOrig <- origAnnot
+  hitAnnotOrig <- hitAnnot
   if(!is.null(which)) hits <- hits[which,]
   for(hitRun in 1:nrow(hits)){
-    if(!is.null(figureFolder)) png(file=paste(figureFolder,figurePrefix,hitRun,".png",sep=""), width=2000, height=1400)
-    if(verbose) cat("Start to create figure at",date(),"\n")
+  # Restore for each run the original values
+    window <- windowOrig
+    origAnnot <- origAnnotOrig
+    hitAnnot <- hitAnnotOrig
+    tempGeneName <-gsub(' \"', '', strsplit(strsplit(hits[hitRun]$V9,"gene_name")[[1]][2],'\";')[[1]][1])
+    # If the previous version hasn't found a gene name, try another way
+    if(is.na(tempGeneName)){
+      tempGeneName <- strsplit(strsplit(hits[hitRun]$V9,"gene=")[[1]][[2]],";")[[1]][[1]]
+    }
+
+    if(!is.null(figureFolder)) png(file=paste(figureFolder,figurePrefix,hitRun,"-",tempGeneName,".png",sep=""), width=2000, height=1400)
+    if(verbose) cat("Start to create figure for",tempGeneName,"(",date(),")\n")
   # Import the fasta files
   # ADD HERE STILL THE OPTION FOR AN OWN FASTA FILE!!!
-    if(verbose) cat("Import the sequence files",date(),"\n")
     if(is.null(fastaFolder)){
       stop("No directory with fasta file given!")    
     } else {
@@ -20,10 +33,12 @@ plotHit <- function(hits, flanking=1, window=NULL, annot=TRUE, coverage=FALSE, l
         temp <- hoardeR::species
         origSpeciesVersion <- temp$Ensembl.Assembly[temp$Scientific.name==origSpecies]
       }
+      if(verbose) cat("Original species version:", origSpeciesVersion,"\n")
       if(is.null(hitSpeciesVersion)){
         temp <- hoardeR::species
         hitSpeciesVersion <- temp$Ensembl.Assembly[temp$Scientific.name==hitSpecies]
       }
+      if(verbose) cat("Hit species version:", hitSpeciesVersion,"\n")
       # First the required original fasta file
         dir.create(fastaFolder, showWarnings=FALSE)
         origSpecies.int <- gsub(" ", "_",tolower(origSpecies))
@@ -31,6 +46,7 @@ plotHit <- function(hits, flanking=1, window=NULL, annot=TRUE, coverage=FALSE, l
         fileName <-  paste(cap(origSpecies.int),".",origSpeciesVersion,".dna.chromosome.",hits$origChr[hitRun],".fa.gz",sep="")
         .file = file.path(fastaFolder, fileName)
         if(!file.exists(.file)) download.file(paste(ensemblURL,fileName,sep=""), .file)
+        if(verbose) cat("Read original fasta file:\n   ", .file ,"\n")
         seqOrig <- read.fasta(.file,seqtype="DNA")
       # And then the hit species    
         hitSpecies.int <- gsub(" ", "_",tolower(hitSpecies))
@@ -38,6 +54,7 @@ plotHit <- function(hits, flanking=1, window=NULL, annot=TRUE, coverage=FALSE, l
         fileName <-  paste(cap(hitSpecies.int),".",hitSpeciesVersion,".dna.chromosome.",hits$hitChr[hitRun],".fa.gz",sep="")
         .file = file.path(fastaFolder, fileName)
         if(!file.exists(.file)) download.file(paste(ensemblURL,fileName,sep=""), .file)
+        if(verbose) cat("Read hit fasta file:\n   ", .file ,"\n")
         seqHit <- read.fasta(.file,seqtype="DNA")
     }
     
@@ -48,6 +65,15 @@ plotHit <- function(hits, flanking=1, window=NULL, annot=TRUE, coverage=FALSE, l
     hitChr <- hits$hitChr[hitRun]
     hitStart <- as.numeric(hits$hitStart[hitRun])
     hitEnd <- as.numeric(hits$hitEnd[hitRun]) 
+    if(verbose){
+      cat("Original loci:\n   ", origChr,":",
+          prettyNum(as.character(origStart), big.mark=","),"-",
+          prettyNum(as.character(origEnd), big.mark=","),"\n")
+      cat("Hit loci:\n   ", hitChr,":",
+          prettyNum(as.character(hitStart), big.mark=","),"-",
+          prettyNum(as.character(hitEnd), big.mark=","),"\n")
+    }
+    
     
     # Getting the flanking coordinates
       ORIGcoord <- c(origStart,origEnd)
@@ -71,11 +97,14 @@ plotHit <- function(hits, flanking=1, window=NULL, annot=TRUE, coverage=FALSE, l
         hitEndFlank <- hitEnd + 1000 * flanking        
       }
     
+      
     # Get the sequence coordiantes
       if(is.null(window)) window <- origEnd-origStart
+      if(verbose) cat("Using window size:", window,"\n")
       origLeft <- seq(origStart,origStartFlank,-window)
       origRight <- seq(origEnd,origEndFlank,window)
-    
+      if(verbose) cat("Slots (width/window) in area:", length(origLeft) + length(origRight) + 1,"\n")
+      
     # Get the plotting coordinates
       xRange <- range(c(origLeft,origRight))
       yRange <- c(-1.25,1.25)
@@ -99,34 +128,43 @@ plotHit <- function(hits, flanking=1, window=NULL, annot=TRUE, coverage=FALSE, l
         HITwinSeqLeft <- c()
         ORIGwinSeqRight <- c()
         HITwinSeqRight <- c()
-        if(verbose) cat("Get the left scores\n")
+        if(verbose) cat("Get the left sequences\n")
+        if(verbose) pb   <- txtProgressBar(1, length(scoresLeft), style=3, width=60)
         for(i in 1:length(scoresLeft)){
           ORIGwinSeqLeft[i] <- paste(seqOrig[[1]][origLeft[length(origLeft)+1-i]:origLeft[length(origLeft)-i]],collapse="")
           HITwinSeqLeft[i] <- paste(seqHit[[1]][hitLeft[length(hitLeft)+1-i]:hitLeft[length(hitLeft)-i]] ,collapse="")
           if(HITneg) HITwinSeqLeft[i] <- paste(rev(comp(strsplit(HITwinSeqLeft[i],"")[[1]])),collapse="")
+          if(verbose) setTxtProgressBar(pb, i)
         }
-        if(verbose) cat("Get the right scores\n")
+        if(verbose) cat("\nGet the right sequences\n")
+        if(verbose) pb   <- txtProgressBar(1, length(scoresRight), style=3, width=60)
         for(i in 1:length(scoresRight)){
           ORIGwinSeqRight[i] <- paste(seqOrig[[1]][origRight[i]:origRight[1+i]], collapse="")  
           HITwinSeqRight[i] <- paste(seqHit[[1]][hitRight[i]:hitRight[i+1]],collapse="")
           if(HITneg) HITwinSeqRight[i] <- paste(rev(comp(strsplit(HITwinSeqRight[i],"")[[1]])),collapse="")
+          if(verbose) setTxtProgressBar(pb, i)
         } 
         origSequence <- paste(seqOrig[[1]][origStart:origEnd],collapse="")
         hitSequence <- paste(seqHit[[1]][hitStart:hitEnd],collapse="")
+        
+        if(HITneg) hitSequence <- paste(comp(strsplit(hitSequence,"")[[1]]),collapse="")
+        
         ORIGwinSeq <- c(ORIGwinSeqLeft,origSequence,ORIGwinSeqRight)
         HITwinSeq <- c(HITwinSeqLeft,hitSequence, HITwinSeqRight)
         scoreMatrix <- matrix(-1,nrow=length(ORIGwinSeq), ncol=6)
         colnames(scoreMatrix) <- c("ORIGstart","ORIGend", "HITstart", "HITwidth", "score", "hitIndex")
-        mat <- nucleotideSubstitutionMatrix(match = 2, 
-                                            mismatch = -2,
+        mat <- nucleotideSubstitutionMatrix(match = 1, 
+                                            mismatch = -1,
                                             baseOnly = FALSE)
+        if(verbose) cat("\nCalculate the scores of",nrow(scoreMatrix),"times",nrow(scoreMatrix),"=",nrow(scoreMatrix)^2,"combinations \n")
+        if(verbose) pb   <- txtProgressBar(1, nrow(scoreMatrix), style=3, width=60)
       for(origIndRun in 1:nrow(scoreMatrix)){
         bestHIT <- 0
         for(hitIndRun in 1:nrow(scoreMatrix)){
           evalThis <- pairwiseAlignment(toupper(ORIGwinSeq[origIndRun]),
                                         toupper(HITwinSeq[hitIndRun]),
-                                        gapOpening=-2,
-                                        gapExtension=-2,
+                                        gapOpening=-1,
+                                        gapExtension=-1,
                                         substitutionMatrix = mat,
                                         type="local")
           if(evalThis@score/(window)>scoreMatrix[origIndRun,5]){
@@ -141,13 +179,13 @@ plotHit <- function(hits, flanking=1, window=NULL, annot=TRUE, coverage=FALSE, l
         scoreMatrix[origIndRun,3] <- xRange[1] + (bestHit-1)*window + best@subject@range@start
         scoreMatrix[origIndRun,4] <- scoreMatrix[origIndRun,3] + best@subject@range@width
         scoreMatrix[origIndRun,6] <- bestHit
-        if(verbose) cat("Row",origIndRun,"of",nrow(scoreMatrix),"processed at",date(),"\n")
+        if(verbose) setTxtProgressBar(pb, origIndRun)
       }
   ## CHECK HERE STILL WITH THE SCORING; WHY ARE THOSE WEIGHTS THERE!?!?!?
       scoresHit <- pairwiseAlignment(toupper(origSequence), 
                                      toupper(hitSequence), 
-                                     gapOpening=-2, 
-                                     gapExtension=-2, 
+                                     gapOpening=-1, 
+                                     gapExtension=-1, 
                                      substitutionMatrix = mat,
                                      type="local")@score
       sch <- scoresHit / (window)
@@ -155,39 +193,40 @@ plotHit <- function(hits, flanking=1, window=NULL, annot=TRUE, coverage=FALSE, l
   #    sch <- (sch + 101)/2
       
     } else {
-      for(i in 1:length(scoresLeft)){
-        if(HITneg){
-          scoresLeft[i] <- getMatch(SSChr=SSChr, SSStart=SSLeft[length(SSLeft)+1-i], SSEnd=SSLeft[length(SSLeft)-i],
-                                    hitChr=hitChr, hitStart=hitRight[length(hitRight)+1-i], hitEnd=hitRight[length(hitRight)-i], hitOrga=hitOrga)@score      
-        } else {
-          scoresLeft[i] <- getMatch(SSChr=SSChr, SSStart=SSLeft[i], SSEnd=SSLeft[i+1],
-                                    hitChr=hitChr, hitStart=hitLeft[i], hitEnd=hitLeft[i+1], hitOrga=hitOrga)@score
-        }
-      }
-      for(i in 1:length(scoresRight)){
-        if(HITneg){
-          scoresRight[i] <- getMatch(SSChr=SSChr, SSStart=SSRight[i], SSEnd=SSRight[1+i],
-                                     hitChr=hitChr, hitStart=hitLeft[length(hitLeft)-i], hitEnd=hitLeft[length(hitLeft)-i+1], hitOrga=hitOrga)@score      
-        } else {
-          scoresRight[i] <- getMatch(SSChr=SSChr, SSStart=SSRight[i], SSEnd=SSRight[i+1],
-                                     hitChr=hitChr, hitStart=hitRight[i], hitEnd=hitRight[i+1], hitOrga=hitOrga)@score
-        }
-      }
-      scoresHit <- getMatch(SSChr=SSChr, SSStart=SSStart, SSEnd=SSEnd,
-                            hitChr=hitChr, hitStart=hitStart, hitEnd=hitEnd, hitOrga=hitOrga)@score      
-      
-      
-      scl <- round(scoresLeft / (2*window)*100)
-      scr <- round(scoresRight / (2*window)*100)
-      sch <- round(scoresHit / (2*window)*100)
-      
-      scl <- (scl + 101)/2
-      scr <- (scr + 101)/2
-      sch <- (sch + 101)/2
+      stop("Currently only diagonal plots are supported!")
+#       for(i in 1:length(scoresLeft)){
+#         if(HITneg){
+#           scoresLeft[i] <- getMatch(SSChr=SSChr, SSStart=SSLeft[length(SSLeft)+1-i], SSEnd=SSLeft[length(SSLeft)-i],
+#                                     hitChr=hitChr, hitStart=hitRight[length(hitRight)+1-i], hitEnd=hitRight[length(hitRight)-i], hitOrga=hitOrga)@score      
+#         } else {
+#           scoresLeft[i] <- getMatch(SSChr=SSChr, SSStart=SSLeft[i], SSEnd=SSLeft[i+1],
+#                                     hitChr=hitChr, hitStart=hitLeft[i], hitEnd=hitLeft[i+1], hitOrga=hitOrga)@score
+#         }
+#       }
+#       for(i in 1:length(scoresRight)){
+#         if(HITneg){
+#           scoresRight[i] <- getMatch(SSChr=SSChr, SSStart=SSRight[i], SSEnd=SSRight[1+i],
+#                                      hitChr=hitChr, hitStart=hitLeft[length(hitLeft)-i], hitEnd=hitLeft[length(hitLeft)-i+1], hitOrga=hitOrga)@score      
+#         } else {
+#           scoresRight[i] <- getMatch(SSChr=SSChr, SSStart=SSRight[i], SSEnd=SSRight[i+1],
+#                                      hitChr=hitChr, hitStart=hitRight[i], hitEnd=hitRight[i+1], hitOrga=hitOrga)@score
+#         }
+#       }
+#       scoresHit <- getMatch(SSChr=SSChr, SSStart=SSStart, SSEnd=SSEnd,
+#                             hitChr=hitChr, hitStart=hitStart, hitEnd=hitEnd, hitOrga=hitOrga)@score      
+#       
+#       
+#       scl <- round(scoresLeft / (2*window)*100)
+#       scr <- round(scoresRight / (2*window)*100)
+#       sch <- round(scoresHit / (2*window)*100)
+#       
+#       scl <- (scl + 101)/2
+#       scr <- (scr + 101)/2
+#       sch <- (sch + 101)/2
     }
     
     
-    plot(-100,-100, ylim=yRange, xlim=c(xRange), xaxt="n", yaxt="n", ylab="", xlab="Chromosomal positions on original and target organism")
+    plot(-100,-100, ylim=yRange, xlim=c(xRange), xaxt="n", yaxt="n", ylab="", xlab="Chromosomal positions of original and target organism")
     
     
     redGreenFun <- colorRampPalette(c("red", "green"))
@@ -196,7 +235,7 @@ plotHit <- function(hits, flanking=1, window=NULL, annot=TRUE, coverage=FALSE, l
     if(is.numeric(diagonal)){
       # Plot the hit polygons
       plotDiag <- scoreMatrix[scoreMatrix[,5]>diagonal,]
-      if(nrow(plotDiag)==0){
+      if(nrow(as.matrix(plotDiag))==0){
         warning("No matches found for 'diagonal' filter setting!")
       } else {
         if(is.vector(plotDiag)){
@@ -210,7 +249,7 @@ plotHit <- function(hits, flanking=1, window=NULL, annot=TRUE, coverage=FALSE, l
       
       
     # Plot the hitbox
-      rect(origStart,-1,origEnd,1,col=redGreen[sch], border=NA)
+      rect(origStart,-1,origEnd,1,col=redGreen[min(99, round((sch * 100)))], border=NA)
     # Plot the other hits in the window
     # Get first the candidates to plot
       otherHits <- hitsOrig[(hitsOrig$hitChr==hitChr) & (hitsOrig$origChr==origChr),]
@@ -275,6 +314,12 @@ plotHit <- function(hits, flanking=1, window=NULL, annot=TRUE, coverage=FALSE, l
   #    lines(c(origStart, hitEnd-hitStart+origStart),c(-1,-1),lwd=5)
   #  }
     
+    if(HITneg){
+      axis(2,at=c(-1,1),label=c("Neg. strand","Pos. strand"))
+    } else {
+      axis(2,at=c(-1,1),label=c("Pos. strand","Pos. strand"))
+    }
+    
     # In case the annot flag is set, add it to the plot
     if(annot){  
       origAnnot <- origAnnot[origAnnot$V1==origChr,]
@@ -290,10 +335,17 @@ plotHit <- function(hits, flanking=1, window=NULL, annot=TRUE, coverage=FALSE, l
       }
   
       hitAnnot <- hitAnnot[hitAnnot$V1==hitChr,]
-      hitAnnot <- hitAnnot[((hitAnnot$V4<=hitEndFlank) & (hitAnnot$V4>=hitStartFlank))  ,]
-      hitAnnot <- hitAnnot[((hitAnnot$V5<=hitEndFlank) & (hitAnnot$V5>=hitStartFlank))  ,]
-      hitAnnot <- hitAnnot[hitAnnot$V3=="gene",]
-      hitRange <- range(c(hitLeft,hitRight))
+      if(HITneg){
+        hitAnnot <- hitAnnot[((hitAnnot$V4>=hitEndFlank) & (hitAnnot$V4<=hitStartFlank))  ,]
+        hitAnnot <- hitAnnot[((hitAnnot$V5>=hitEndFlank) & (hitAnnot$V5<=hitStartFlank))  ,]
+        hitAnnot <- hitAnnot[hitAnnot$V3=="gene",]
+        hitRange <- range(c(hitLeft,hitRight))
+      } else {
+        hitAnnot <- hitAnnot[((hitAnnot$V4<=hitEndFlank) & (hitAnnot$V4>=hitStartFlank))  ,]
+        hitAnnot <- hitAnnot[((hitAnnot$V5<=hitEndFlank) & (hitAnnot$V5>=hitStartFlank))  ,]
+        hitAnnot <- hitAnnot[hitAnnot$V3=="gene",]
+        hitRange <- range(c(hitLeft,hitRight))
+      }
       
       if(nrow(hitAnnot)>0){
   #      if(is.numeric(exonMatch)){
@@ -302,6 +354,10 @@ plotHit <- function(hits, flanking=1, window=NULL, annot=TRUE, coverage=FALSE, l
         for(i in 1:nrow(hitAnnot)){
           lines(c(hitAnnot$V4[i]+ xRange[1] - hitRange[1], hitAnnot$V5[i] + xRange[1]-hitRange[1]), c(-1.5,-1.5), lwd=5)
           tempGeneName <-gsub(' \"', '', strsplit(strsplit(hitAnnot$V9[i],"gene_name")[[1]][2],'\";')[[1]][1])
+        # If the previous version hasn't found a gene name, try another way
+          if(is.na(tempGeneName)){
+            tempGeneName <- strsplit(strsplit(hitAnnot$V9[i],"gene=")[[1]][[2]],";")[[1]][[1]]
+          }
           text(mean(c(hitAnnot$V4[i]+ xRange[1] - hitRange[1], hitAnnot$V5[i] + xRange[1]-hitRange[1])), -1.6, tempGeneName) 
           
     #      temp <- as.character(HITexon$gene_id)
@@ -364,13 +420,14 @@ plotHit <- function(hits, flanking=1, window=NULL, annot=TRUE, coverage=FALSE, l
       # lines(xRange,c(3,3))
       # Y-Axis Label
       if(HITneg){
-        axis(2,at=c(-1,1,2,3),label=c("Neg. strand","Pos. strand","0",maxCounts))
+        axis(2,at=c(2,3),label=c("0",maxCounts))
       } else {
-        axis(2,at=c(-1,1,2,3),label=c("Pos. strand","Pos. strand","0",maxCounts))
+        axis(2,at=c(2,3),label=c("0",maxCounts))
       }
     }
     
     if(output) list(SHit=scoresHit, SL=scoresLeft,SR=scoresRight, scoreMat=scoreMatrix)
     if(!is.null(figureFolder)) dev.off()
+    if(verbose) cat("\nFigure created (",date(),")\n")
   }
 }
