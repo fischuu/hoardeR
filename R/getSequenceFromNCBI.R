b5ee272aae3841d13ac4120e7f128570dd025f6c
fischuu <github@danielfischer.name>
2018-05-07 21:02:14 +0300

    linebreaks removed

--- R/getSequenceFromNCBI.R
+++ R/getSequenceFromNCBI.R
@@ -1,8 +1,14 @@
 getSequenceFromNCBI <- function(id, file=NULL){
   out <- c()
   for(idRun in 1:length(id)){
-    out[idRun] <- getURL(paste("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=",id[idRun],"&rettype=fasta&retmode=text", sep=""))    
-    if(idRun%%10==0) message("Processed",idRun,"queries.")
+    
+    tmp <- getURL(paste("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=",id[idRun],"&rettype=fasta&retmode=text", sep=""))    
+    tmp <- sub("\n","\r",tmp)
+    tmp <- gsub("\n","",tmp)
+    tmp <- sub("\r","\n",tmp)
+    out[idRun] <- tmp
+    
+    if(idRun%%10==0) message("Processed ",idRun," queries.")
     if(idRun==length(id)) message("All queries processed!")
   }
 
