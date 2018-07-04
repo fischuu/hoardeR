f837c9d54adb16089315690c0236c91ae622d913
fischuu <git@danielfischer.name>
2018-07-04 09:06:45 +0300

    importFQ added (pluf print.fq and summary.fq)

---
+++ R/importFQ.R
@@ -0,0 +1,14 @@
+# This function reads in a fasta file and prepares the vector from it
+  importFQ <- function(file){
+    res <- readLines(file)
+  # Check if the Fasta file is alternating, one line label, the next line sequence
+    sumAlternating <- sum(grepl("@",res)==c(TRUE,FALSE,FALSE,FALSE))
+    if(sumAlternating!=length(res)) stop("Your Fastq file is malformed. Please ensure that name rows start with > and that names and sequence
+                                         rows are alternating (plus the two additional rows for the quality).")
+    seq <- res[seq(2,length(res),4)]
+    qual <- res[seq(4,length(res),4)]
+    names(seq) <- res[seq(1,length(res)-1,4)]
+    names(qual) <- res[seq(1,length(res)-1,4)]
+    class(seq) <- "fa"
+    seq
+} 
---
+++ R/summary.fq.R
@@ -0,0 +1,15 @@
+summary.fq <- function(object, ...){
+
+   nCharObj <- nchar(object)
+   cat("Summary of fq object\n")
+   cat("--------------------\n")
+   cat("Sequences      :",length(object),"\n")
+   cat("Minimum length :",min(nCharObj),"\n")
+   cat("1st quartile   :",quantile(nCharObj, 0.25),"\n")
+   cat("Median length  :",median(nCharObj),"\n")
+   cat("Average length :",mean(nCharObj),"\n")
+   cat("3rd quartile   :",quantile(nCharObj, 0.75),"\n")
+   cat("Maximum length :",max(nCharObj),"\n")
+   invisible(object)
+
+} 
