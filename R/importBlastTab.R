1dc4654927adfe01f66ac77fa13e061d7863efbe
fischuu <git@danielfischer.name>
2019-02-06 15:18:52 +0200

    Old files deleted

--- R/importBlastTab.R
+++
@@ -1,7 +0,0 @@
-importBlastTab <- function(file){
-
-  .Deprecated("GenomicTools.fileHandler::importBlastTab", package="GenomicTools", msg="I/O Functions will be collected from now on in a new package GenomicTools.fileHandler")
-  
-  out <- read.table(file,stringsAsFactors=FALSE, sep="\t")
-  out
-}
